c82e181c9671bb9fe24583a6e073de7e9484b249
Hellguy <129449907+helguyuser1@users.noreply.github.com>
2024-08-18 19:39:24 -0300

    Refresh.lua

---
+++ scripts/Refresh.lua
@@ -0,0 +1,18 @@
+--Work R6 & R15 
+local plr = game.Players.LocalPlayer
+local hrp = plr.Character:WaitForChild("HumanoidRootPart")
+local lastPos = hrp.Position
+
+plr.Character:WaitForChild("Humanoid").Health = 0
+
+local connection
+
+connection = plr.CharacterAdded:Connect(function(char)
+    local humanoidRootPart = char:WaitForChild("HumanoidRootPart")
+
+    repeat wait() until humanoidRootPart.Parent ~= nil
+
+    humanoidRootPart.CFrame = CFrame.new(lastPos)
+    
+    connection:Disconnect()
+end)
