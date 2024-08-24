eac53e72be5d65dcdca0280e67559834bc56dfea
Hellguy <129449907+helguyuser1@users.noreply.github.com>
2024-08-23 22:04:01 -0300

    Update PastebinOnlinePatch.lua

--- scripts/PastebinOnlinePatch.lua
+++ scripts/PastebinOnlinePatch.lua
@@ -5,6 +5,7 @@ if _G.Api ~= "false" then
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Expired!",
         Text = _G.Discord,
+        Icon = "rbxassetid://5107155560",
         Duration = 3,
         Button1 = "Copy Discord"
     })
